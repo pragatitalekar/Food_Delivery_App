fae57750dd0f28fe0538bb2fbe81bcf068970ca0
pragatitalekar <pragatitalekar@users.noreply.github.com>
2026-02-04 21:40:12 -0800

    Initial commit

---
+++ Food_Delivery_App/Food_Delivery_AppApp.swift
@@ -0,0 +1,32 @@
+//
+//  Food_Delivery_AppApp.swift
+//  Food_Delivery_App
+//
+//  Created by rentamac on 2/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Food_Delivery_AppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Food_Delivery_App/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Food_Delivery_App
+//
+//  Created by rentamac on 2/4/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
