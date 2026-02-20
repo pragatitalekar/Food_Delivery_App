b7e5239ca4fcebf1b1ac83d4a8adee949c97625b
KiranGanachari1801 <kiranvganachari@gmail.com>
2026-02-19 20:57:13 -0800

    homeview chnaged

---
+++ Food_Delivery_App/Features/Cart/Views/EmptyCartView.swift
@@ -0,0 +1,18 @@
+//
+//  EmptyCartView.swift
+//  Food_Delivery_App
+//
+//  Created by rentamac on 2/19/26.
+//
+
+import SwiftUI
+
+struct EmptyCartView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmptyCartView()
+}
